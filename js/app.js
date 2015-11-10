31ebe0e71976485db2f30350cf23648f990330ae
thfield <thfield@gmail.com>
2015-11-10 10:02:58 -0800

    reusable works

--- js/app.js
+++ js/app.js
@@ -1,69 +1,91 @@
+function apportionMap() {
+  var mult = 7,
+      margin = {top: 20, right: 20, bottom: 20, left: 20},
+      width = 400,
+      height = 250;
 
-var mult = 7;
+  var circledata = [{state: "CA",lat: 37.19,long: -119.45,reps: 53}, {state: "TX",lat: 31.47,long: -99.32,reps: 36}, {state: "FL",lat: 28.62,long: -82.44,reps: 27}, {state: "NY",lat: 42.95,long: -75.52,reps: 27}, {state: "IL",lat: 40.04,long: -89.19,reps: 18}, {state: "PA",lat: 40.87,long: -77.79,reps: 18}, {state: "OH",lat: 40.28,long: -82.79,reps: 16}, {state: "GA",lat: 32.64,long: -83.44,reps: 14}, {state: "MI",lat: 44.34,long: -85.41,reps: 14}, {state: "NC",lat: 35.55,long: -79.38,reps: 13}, {state: "NJ",lat: 40.19,long: -74.67,reps: 12}, {state: "VA",lat: 37.52,long: -78.85,reps: 11}, {state: "WA",lat: 47.38,long: -120.45,reps: 10}, {state: "AZ",lat: 34.27,long: -111.65,reps: 9}, {state: "IN",lat: 39.89,long: -86.28,reps: 9}, {state: "MA",lat: 42.25,long: -71.8,reps: 9}, {state: "TN",lat: 35.86,long: -86.35,reps: 9}, {state: "MD",lat: 39.05,long: -76.79,reps: 8}, {state: "MN",lat: 46.28,long: -94.3,reps: 8}, {state: "MO",lat: 38.35,long: -92.45,reps: 8}, {state: "WI",lat: 44.62,long: -89.99,reps: 8}, {state: "AL",lat: 32.77,long: -86.92,reps: 7}, {state: "CO",lat: 38.99,long: -105.54,reps: 7}, {state: "SC",lat: 33.91,long: -80.89,reps: 7}, {state: "KY",lat: 37.53,long: -85.3,reps: 6}, {state: "LA",lat: 31.06,long: -91.99,reps: 6}, {state: "CT",lat: 41.62,long: -72.72,reps: 5}, {state: "OK",lat: 35.59,long: -97.49,reps: 5}, {state: "OR",lat: 43.93,long: -120.56,reps: 5}, {state: "AR",lat: 34.89,long: -92.44,reps: 4}, {state: "IA",lat: 42.07,long: -93.49,reps: 4}, {state: "KS",lat: 38.49,long: -98.38,reps: 4}, {state: "MS",lat: 32.73,long: -89.66,reps: 4}, {state: "NV",lat: 39.33,long: -116.62,reps: 4}, {state: "UT",lat: 39.3,long: -111.66,reps: 4}, {state: "NE",lat: 41.53,long: -99.79,reps: 3}, {state: "NM",lat: 34.4,long: -106.11,reps: 3}, {state: "WV",lat: 38.64,long: -80.62,reps: 3}, {state: "HI",lat: 20.29,long: -156.37,reps: 2}, {state: "ID",lat: 44.35,long: -114.6,reps: 2}, {state: "ME",lat: 45.37,long: -69.24,reps: 2}, {state: "NH",lat: 43.68,long: -71.58,reps: 2}, {state: "RI",lat: 41.67,long: -71.55,reps: 2}, {state: "AK",lat: 64.06,long: -152.23,reps: 1}, {state: "DE",lat: 38.98,long: -75.5,reps: 1}, {state: "DC",lat: 38.91,long: -77.01,reps: 1}, {state: "MT",lat: 47.05,long: -109.64,reps: 1}, {state: "ND",lat: 47.44,long: -100.46,reps: 1}, {state: "SD",lat: 44.44,long: -100.22,reps: 1}, {state: "VT",lat: 44.06,long: -72.66,reps: 1}, {state: "WY",lat: 42.99,long: -107.55,reps: 1}];
 
-var width = 400,
-    height = 250;
+  function chart(selection) {
+    selection.each(function(data) {
 
-var projection = d3.geo.albersUsa()
-        .scale(width)
-        .translate([width/2, height/2]);
+      var projection = d3.geo.albersUsa()
+              .scale(width)
+              .translate([width/2, height/2]);
+      var path = d3.geo.path().projection(projection);
 
-var path = d3.geo.path().projection(projection);
+      var svg = d3.select(this).append("svg")
+          .attr("width", width)
+          .attr("height", height);
 
-// d3.select("#map").append('div').attr('id','tooltip').attr('class','hidden')
-//   .append('span').attr('id','value');
+      // d3.json("maps/usa-simple.json", function(us) {
+      //   svg.append("path")
+      //         .datum(topojson.feature(us, us.objects.states))
+      //         .attr("d", path)
+      //         .attr("class", "geo");
+      //   d3.select("#force-map svg").append("path")
+      //         .datum(topojson.feature(us, us.objects.states))
+      //         .attr("d", path)
+      //         .attr("class", "geo");
+      // });
 
-var svg = d3.select("#map").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+      // d3.json("data/colors.json", function(data) {
+          var state = svg.selectAll("state")
+              .data(circledata)
+            .enter().append("g").attr("class", "state")
+              .attr("transform", function(d) { return "translate(" + projection([d.long,d.lat]) + ")"; });
 
-// d3.json("maps/usa-simple.json", function(us) {
-//   svg.append("path")
-//         .datum(topojson.feature(us, us.objects.states))
-//         .attr("d", path)
-//         .attr("class", "geo");
-//   d3.select("#force-map svg").append("path")
-//         .datum(topojson.feature(us, us.objects.states))
-//         .attr("d", path)
-//         .attr("class", "geo");
-// });
+          state.append("circle")
+              .attr("class", function(d) { return d.state + ' ' + data[d.state] })
+              .attr("r", function(d) { return Math.sqrt(mult * +d.reps) })
+              .on("mouseover", function(d) {
+                d3.select(this).classed('highlight',true)
+                ttShow(d.state);
+              })
+              .on("mouseout", function(d){
+                d3.select(this).classed('highlight', false)
+                ttHide();
+              });
 
-var circledata = [{state: "CA",lat: 37.19,long: -119.45,reps: 53}, {state: "TX",lat: 31.47,long: -99.32,reps: 36}, {state: "FL",lat: 28.62,long: -82.44,reps: 27}, {state: "NY",lat: 42.95,long: -75.52,reps: 27}, {state: "IL",lat: 40.04,long: -89.19,reps: 18}, {state: "PA",lat: 40.87,long: -77.79,reps: 18}, {state: "OH",lat: 40.28,long: -82.79,reps: 16}, {state: "GA",lat: 32.64,long: -83.44,reps: 14}, {state: "MI",lat: 44.34,long: -85.41,reps: 14}, {state: "NC",lat: 35.55,long: -79.38,reps: 13}, {state: "NJ",lat: 40.19,long: -74.67,reps: 12}, {state: "VA",lat: 37.52,long: -78.85,reps: 11}, {state: "WA",lat: 47.38,long: -120.45,reps: 10}, {state: "AZ",lat: 34.27,long: -111.65,reps: 9}, {state: "IN",lat: 39.89,long: -86.28,reps: 9}, {state: "MA",lat: 42.25,long: -71.8,reps: 9}, {state: "TN",lat: 35.86,long: -86.35,reps: 9}, {state: "MD",lat: 39.05,long: -76.79,reps: 8}, {state: "MN",lat: 46.28,long: -94.3,reps: 8}, {state: "MO",lat: 38.35,long: -92.45,reps: 8}, {state: "WI",lat: 44.62,long: -89.99,reps: 8}, {state: "AL",lat: 32.77,long: -86.92,reps: 7}, {state: "CO",lat: 38.99,long: -105.54,reps: 7}, {state: "SC",lat: 33.91,long: -80.89,reps: 7}, {state: "KY",lat: 37.53,long: -85.3,reps: 6}, {state: "LA",lat: 31.06,long: -91.99,reps: 6}, {state: "CT",lat: 41.62,long: -72.72,reps: 5}, {state: "OK",lat: 35.59,long: -97.49,reps: 5}, {state: "OR",lat: 43.93,long: -120.56,reps: 5}, {state: "AR",lat: 34.89,long: -92.44,reps: 4}, {state: "IA",lat: 42.07,long: -93.49,reps: 4}, {state: "KS",lat: 38.49,long: -98.38,reps: 4}, {state: "MS",lat: 32.73,long: -89.66,reps: 4}, {state: "NV",lat: 39.33,long: -116.62,reps: 4}, {state: "UT",lat: 39.3,long: -111.66,reps: 4}, {state: "NE",lat: 41.53,long: -99.79,reps: 3}, {state: "NM",lat: 34.4,long: -106.11,reps: 3}, {state: "WV",lat: 38.64,long: -80.62,reps: 3}, {state: "HI",lat: 20.29,long: -156.37,reps: 2}, {state: "ID",lat: 44.35,long: -114.6,reps: 2}, {state: "ME",lat: 45.37,long: -69.24,reps: 2}, {state: "NH",lat: 43.68,long: -71.58,reps: 2}, {state: "RI",lat: 41.67,long: -71.55,reps: 2}, {state: "AK",lat: 64.06,long: -152.23,reps: 1}, {state: "DE",lat: 38.98,long: -75.5,reps: 1}, {state: "DC",lat: 38.91,long: -77.01,reps: 1}, {state: "MT",lat: 47.05,long: -109.64,reps: 1}, {state: "ND",lat: 47.44,long: -100.46,reps: 1}, {state: "SD",lat: 44.44,long: -100.22,reps: 1}, {state: "VT",lat: 44.06,long: -72.66,reps: 1}, {state: "WY",lat: 42.99,long: -107.55,reps: 1}];
+          // state.append('text')
+          //     .text(function (d){return d.state })
+          //     .attr("y",  mult/4 );
+        // })
+      });
+    }
 
-d3.json("data/colors.json", function(color) {
-    var state = svg.selectAll("state")
-        .data(circledata)
-      .enter().append("g").attr("class", "state")
-        .attr("transform", function(d) { return "translate(" + projection([d.long,d.lat]) + ")"; });
+  function ttShow(val) {
+    var xPosition = d3.event.pageX;
+    var yPosition = d3.event.pageY;
+    d3.select("#tooltip")
+      .style("left", xPosition + "px")
+      .style("top", yPosition + "px")
+      .select("#tooltip .value")
+      .text(val);
+    d3.select("#tooltip").classed("hidden", false);
+  }
 
-    state.append("circle")
-        .attr("class", function(d) { return d.state + ' ' + color[d.state] })
-        .attr("r", function(d) { return Math.sqrt(mult * +d.reps) })
-        .on("mouseover", function(d) {
-          d3.select(this).classed('highlight',true)
-          ttShow(d.state);
-        })
-        .on("mouseout", function(d){
-          d3.select(this).classed('highlight', false)
-          ttHide();
-        });
+  function ttHide() {
+    d3.select("#tooltip").classed("hidden", true);
+  }
 
-    // state.append('text')
-    //     .text(function (d){return d.state })
-    //     .attr("y",  mult/4 );
-  })
+  chart.margin = function(_) {
+    if (!arguments.length) return margin;
+    margin = _;
+    return chart;
+  };
 
-function ttShow(val) {
-  var xPosition = d3.event.pageX;
-  var yPosition = d3.event.pageY;
-  d3.select("#tooltip")
-    .style("left", xPosition + "px")
-    .style("top", yPosition + "px")
-    .select("#tooltip .value")
-    .text(val);
-  d3.select("#tooltip").classed("hidden", false);
-}
+  chart.width = function(_) {
+    if (!arguments.length) return width;
+    width = _;
+    return chart;
+  };
 
-function ttHide() {
-  d3.select("#tooltip").classed("hidden", true);
+  chart.height = function(_) {
+    if (!arguments.length) return height;
+    height = _;
+    return chart;
+  };
+
+  return chart;
 }
--- js/force.js
+++ js/force.js
@@ -1,85 +1,103 @@
+function apportionForceMap() {
+  var width = 1000,
+      height = 600;
 
-//need to wrap these 2 functions in a closure.  mult is bleeding over from app.js
+  var foci = [{"state":"CA","reps":53,"x":0.1885,"y":0.4827},{"state":"TX","reps":36,"x":0.4589,"y":0.6685},{"state":"FL","reps":27,"x":0.7156,"y":0.7163},{"state":"NY","reps":27,"x":0.7662,"y":0.3631},{"state":"IL","reps":18,"x":0.5982,"y":0.4642},{"state":"PA","reps":18,"x":0.745,"y":0.4187},{"state":"OH","reps":16,"x":0.6819,"y":0.4468},{"state":"GA","reps":14,"x":0.6911,"y":0.6261},{"state":"MI","reps":14,"x":0.6396,"y":0.3581},{"state":"NC","reps":13,"x":0.7408,"y":0.5468},{"state":"NJ","reps":12,"x":0.7876,"y":0.423},{"state":"VA","reps":11,"x":0.7419,"y":0.4996},{"state":"WA","reps":10,"x":0.2206,"y":0.2476},{"state":"AZ","reps":9,"x":0.2851,"y":0.5796},{"state":"IN","reps":9,"x":0.6368,"y":0.4633},{"state":"MA","reps":9,"x":0.8153,"y":0.3643},{"state":"TN","reps":9,"x":0.6431,"y":0.5576},{"state":"MD","reps":8,"x":0.7642,"y":0.4572},{"state":"MN","reps":8,"x":0.5286,"y":0.3224},{"state":"MO","reps":8,"x":0.5562,"y":0.5071},{"state":"WI","reps":8,"x":0.5825,"y":0.3582},{"state":"AL","reps":7,"x":0.6404,"y":0.6308},{"state":"CO","reps":7,"x":0.3801,"y":0.4846},{"state":"SC","reps":7,"x":0.7244,"y":0.5895},{"state":"KY","reps":6,"x":0.6544,"y":0.5165},{"state":"LA","reps":6,"x":0.5678,"y":0.6775},{"state":"CT","reps":5,"x":0.8065,"y":0.3825},{"state":"OK","reps":5,"x":0.4871,"y":0.5729},{"state":"OR","reps":5,"x":0.204,"y":0.3247},{"state":"AR","reps":4,"x":0.5586,"y":0.5883},{"state":"IA","reps":4,"x":0.5404,"y":0.4204},{"state":"KS","reps":4,"x":0.4759,"y":0.5044},{"state":"MS","reps":4,"x":0.6006,"y":0.6361},{"state":"NV","reps":4,"x":0.2345,"y":0.4455},{"state":"UT","reps":4,"x":0.2995,"y":0.463},{"state":"NE","reps":3,"x":0.459,"y":0.4323},{"state":"NM","reps":3,"x":0.3639,"y":0.5908},{"state":"WV","reps":3,"x":0.7148,"y":0.4791},{"state":"HI","reps":2,"x":0.3548,"y":0.774},{"state":"ID","reps":2,"x":0.2781,"y":0.3375},{"state":"ME","reps":2,"x":0.8318,"y":0.2827},{"state":"NH","reps":2,"x":0.8116,"y":0.331},{"state":"RI","reps":2,"x":0.821,"y":0.3763},{"state":"AK","reps":1,"x":0.2041,"y":0.7226},{"state":"DE","reps":1,"x":0.7814,"y":0.454},{"state":"DC","reps":1,"x":0.7618,"y":0.4612},{"state":"MT","reps":1,"x":0.3457,"y":0.2892},{"state":"ND","reps":1,"x":0.4551,"y":0.2941},{"state":"SD","reps":1,"x":0.4557,"y":0.3639},{"state":"VT","reps":1,"x":0.7969,"y":0.3269},{"state":"WY","reps":1,"x":0.3618,"y":0.3875}];
 
-var width = 1000,
-    height = 600;
+  function chart(selection) {
+    selection.each(function(data) {
+      var mult = width * .02,
+          nodes = d3.range(foci.length).map(function(i) {
+            return {id:i, state:foci[i].state, reps:foci[i].reps, x:foci[i].x*width, y:foci[i].y*height};
+          });
+      foci.forEach(function(el){
+        el.x = el.x*width;
+        el.y = el.y*height;
+      })
 
-var mult = width * .02 ;
+      var svg = d3.select(this).append("svg")
+          .attr("width", width)
+          .attr("height", height);
 
-var fill = d3.scale.category10();
+      var force = d3.layout.force()
+          .nodes(nodes)
+          .links([])
+          .charge(-100)
+          .chargeDistance(mult*7)
+          .gravity(0)
+          .size([width, height])
+          .on("tick", tick);
 
-var foci = [{"state":"CA","reps":53,"x":0.1885,"y":0.4827},{"state":"TX","reps":36,"x":0.4589,"y":0.6685},{"state":"FL","reps":27,"x":0.7156,"y":0.7163},{"state":"NY","reps":27,"x":0.7662,"y":0.3631},{"state":"IL","reps":18,"x":0.5982,"y":0.4642},{"state":"PA","reps":18,"x":0.745,"y":0.4187},{"state":"OH","reps":16,"x":0.6819,"y":0.4468},{"state":"GA","reps":14,"x":0.6911,"y":0.6261},{"state":"MI","reps":14,"x":0.6396,"y":0.3581},{"state":"NC","reps":13,"x":0.7408,"y":0.5468},{"state":"NJ","reps":12,"x":0.7876,"y":0.423},{"state":"VA","reps":11,"x":0.7419,"y":0.4996},{"state":"WA","reps":10,"x":0.2206,"y":0.2476},{"state":"AZ","reps":9,"x":0.2851,"y":0.5796},{"state":"IN","reps":9,"x":0.6368,"y":0.4633},{"state":"MA","reps":9,"x":0.8153,"y":0.3643},{"state":"TN","reps":9,"x":0.6431,"y":0.5576},{"state":"MD","reps":8,"x":0.7642,"y":0.4572},{"state":"MN","reps":8,"x":0.5286,"y":0.3224},{"state":"MO","reps":8,"x":0.5562,"y":0.5071},{"state":"WI","reps":8,"x":0.5825,"y":0.3582},{"state":"AL","reps":7,"x":0.6404,"y":0.6308},{"state":"CO","reps":7,"x":0.3801,"y":0.4846},{"state":"SC","reps":7,"x":0.7244,"y":0.5895},{"state":"KY","reps":6,"x":0.6544,"y":0.5165},{"state":"LA","reps":6,"x":0.5678,"y":0.6775},{"state":"CT","reps":5,"x":0.8065,"y":0.3825},{"state":"OK","reps":5,"x":0.4871,"y":0.5729},{"state":"OR","reps":5,"x":0.204,"y":0.3247},{"state":"AR","reps":4,"x":0.5586,"y":0.5883},{"state":"IA","reps":4,"x":0.5404,"y":0.4204},{"state":"KS","reps":4,"x":0.4759,"y":0.5044},{"state":"MS","reps":4,"x":0.6006,"y":0.6361},{"state":"NV","reps":4,"x":0.2345,"y":0.4455},{"state":"UT","reps":4,"x":0.2995,"y":0.463},{"state":"NE","reps":3,"x":0.459,"y":0.4323},{"state":"NM","reps":3,"x":0.3639,"y":0.5908},{"state":"WV","reps":3,"x":0.7148,"y":0.4791},{"state":"HI","reps":2,"x":0.3548,"y":0.774},{"state":"ID","reps":2,"x":0.2781,"y":0.3375},{"state":"ME","reps":2,"x":0.8318,"y":0.2827},{"state":"NH","reps":2,"x":0.8116,"y":0.331},{"state":"RI","reps":2,"x":0.821,"y":0.3763},{"state":"AK","reps":1,"x":0.2041,"y":0.7226},{"state":"DE","reps":1,"x":0.7814,"y":0.454},{"state":"DC","reps":1,"x":0.7618,"y":0.4612},{"state":"MT","reps":1,"x":0.3457,"y":0.2892},{"state":"ND","reps":1,"x":0.4551,"y":0.2941},{"state":"SD","reps":1,"x":0.4557,"y":0.3639},{"state":"VT","reps":1,"x":0.7969,"y":0.3269},{"state":"WY","reps":1,"x":0.3618,"y":0.3875}],
-    nodes = d3.range(foci.length).map(function(i) {
-      return {id:i, state:foci[i].state, reps:foci[i].reps, x:foci[i].x*width, y:foci[i].y*height};
+      var node = svg.selectAll("circle");
+
+      function tick(e) {
+        var k = 1 * e.alpha;
+        // Push nodes toward their designated focus.
+        nodes.forEach(function(o, i) {
+          o.y += (foci[o.id].y - o.y) * k;
+          o.x += (foci[o.id].x - o.x) * k;
+        });
+        node
+          .attr("cx", function(d) { return d.x; })
+          .attr("cy", function(d) { return d.y; });
+      }
+
+      var n = 500;
+      force.start();
+      // for (var i = 0; i < n; ++i) force.tick();
+      // force.stop();
+
+
+    node = node.data(nodes);
+    node.enter().append("circle")
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
+        .attr("r", function(d,i) { return Math.sqrt(d.reps*mult); })
+        .attr("class", function(d) { return d.state + ' ' + data[d.state] })
+        .on("mouseover", function(d) {
+          d3.select(this).classed('highlight',true)
+          ttShow(d.state);
+        })
+        .on("mouseout", function(d){
+          d3.select(this).classed('highlight', false)
+          ttHide();
+        });
+        // .call(force.drag);
+
     });
-foci.forEach(function(el){
-  el.x = el.x*width;
-  el.y = el.y*height;
-})
+  }
 
-var svg = d3.select("#force-map").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+  function ttShow(val) {
+    var xPosition = d3.event.pageX;
+    var yPosition = d3.event.pageY;
+    d3.select("#tooltip")
+      .style("left", xPosition + "px")
+      .style("top", yPosition + "px")
+      .select("#tooltip .value")
+      .text(val);
+    d3.select("#tooltip").classed("hidden", false);
+  }
 
-var force = d3.layout.force()
-    .nodes(nodes)
-    .links([])
-    .charge(-60)
-    .chargeDistance(mult*7)
-    .gravity(0)
-    .size([width, height])
-    .on("tick", tick);
+  function ttHide() {
+    d3.select("#tooltip").classed("hidden", true);
+  }
+  chart.margin = function(_) {
+    if (!arguments.length) return margin;
+    margin = _;
+    return chart;
+  };
 
-var node = svg.selectAll("circle");
+  chart.width = function(_) {
+    if (!arguments.length) return width;
+    width = _;
+    return chart;
+  };
 
-function tick(e) {
-  var k = 1 * e.alpha;
-  // Push nodes toward their designated focus.
-  nodes.forEach(function(o, i) {
-    o.y += (foci[o.id].y - o.y) * k;
-    o.x += (foci[o.id].x - o.x) * k;
-  });
-  node
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
-}
+  chart.height = function(_) {
+    if (!arguments.length) return height;
+    height = _;
+    return chart;
+  };
 
-var n = 500;
-force.start();
-// for (var i = 0; i < n; ++i) force.tick();
-// force.stop();
-
-d3.json("data/colors.json", function(color) {
-  node = node.data(nodes);
-  node.enter().append("circle")
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; })
-      .attr("r", function(d,i) { return Math.sqrt(d.reps*mult); })
-      // .style("fill", function(d) { return fill(d.id); })
-      .attr("class", function(d) { return d.state + ' ' + color[d.state] })
-      .on("mouseover", function(d) {
-        d3.select(this).classed('highlight',true)
-        ttShow(d.state);
-      })
-      .on("mouseout", function(d){
-        d3.select(this).classed('highlight', false)
-        ttHide();
-      })
-      ;
-      // .call(force.drag);
-});
-
-function ttShow(val) {
-  var xPosition = d3.event.pageX;
-  var yPosition = d3.event.pageY;
-  d3.select("#tooltip")
-    .style("left", xPosition + "px")
-    .style("top", yPosition + "px")
-    .select("#tooltip .value")
-    .text(val);
-  d3.select("#tooltip").classed("hidden", false);
-}
-
-function ttHide() {
-  d3.select("#tooltip").classed("hidden", true);
+  return chart;
 }
